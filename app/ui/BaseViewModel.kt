8b91d1d02d7d4f15180465c5afbda54f0a8584be
Esma3el Nabil <esmaeelnabil@yahoo.com>
2020-08-07 01:58:37 +0200

    <

--- app/ui/BaseViewModel.kt
+++
@@ -1,9 +0,0 @@
-package
-
-import android.app.Application
-import androidx.lifecycle.ViewModel
-import io.reactivex.disposables.CompositeDisposable
-
-abstract class BaseViewModel() : ViewModel() {
-    val compositeDisposable = CompositeDisposable()
-}
